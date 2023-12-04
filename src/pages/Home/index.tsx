051016e7aacd2152905f772c47007c631aa3dd58
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-12-04 15:28:09 -0300

    Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em:</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="">Durante</label>
+          <input type="number" id="minutes/mount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
